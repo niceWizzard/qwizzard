27b1b5bebdb213b4a50c4b88b40a62f9ce69ac1e
Richard <richard232manansala@gmail.com>
2024-04-11 20:34:12 +0800

    Added conditional rendering to Login link & made it a UButton

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
+
+
+
   const {data,status, signOut} = useAuth()
+
+  function canRenderLoginBtn() {
+    return status.value !== 'authenticated' && 
+           useRouter().currentRoute.value.fullPath.includes('login') === false
+  }
 
 </script>
 
@@ -7,7 +15,7 @@
   <header class="bg-blue-300 min-h-[7vh] p-3 flex flex-row justify-between">
     <h1>Quizzard</h1>
     <div class="flex flex-row gap-6">
-      <a href="/login" v-if="status == 'unauthenticated'">Login</a>
+      <UButton to="/login" v-if="canRenderLoginBtn()">Login</UButton>
       <button  v-on:click="signOut()"
       v-if="status == 'authenticated'">Logout</button>
       <a href="/login" v-if="status == 'authenticated'">Create Quiz</a>
