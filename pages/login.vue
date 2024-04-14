0d8ef4c308772bd88cb47f4a23b749b593b98d2b
Richard <richard232manansala@gmail.com>
2024-04-14 19:45:23 +0800

    Deisgned the sign in page

--- pages/login.vue
+++ pages/login.vue
@@ -10,18 +10,18 @@
   async function signInWithCredentials() {
     try {
       await signIn("github")
-      alert('Successfully logged in!')
     } catch (error) {
+      alert("Error when logging in!");
       console.error(error)
     }
   }
-  const {data } = useAuth();
 
 </script>
 <template>
-  <div>
-    <span>{{ data?.user }}</span>
-    <h1>Enter your credentials to continue</h1>
-    <button @click="signInWithCredentials()">Sign in with Github</button>
+  <div class="grid place-content-center">
+    <div class="border border-main border-r-2 p-3 grid place-content-center gap-3">
+        <h3 class="text-center font-bold text-xl">Join us!</h3>
+        <UButton color="primary" variant="outline" v-on:click="signInWithCredentials()">Login with Github</UButton>
+    </div>
   </div>
 </template>
